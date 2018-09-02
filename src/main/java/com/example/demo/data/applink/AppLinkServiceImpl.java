ef62dbd62b1c2db1121c5f45a2936055c5ef9891
dev-demos <ejbgames@gmail.com>
2018-09-02 14:33:19 -0500

    Include index.html

--- src/main/java/com/example/demo/data/applink/AppLinkServiceImpl.java
+++ src/main/java/com/example/demo/data/applink/AppLinkServiceImpl.java
@@ -44,7 +44,17 @@ public class AppLinkServiceImpl implements AppLinkService {
         alRepo.save(new AppLink("Wisc", "UW homepage", "Blue", false, new URL("http://www.wisc.edu")));
         alRepo.save(new AppLink("GLBRC", "Great Lakes Bioenergy Research Center", "Yellow", true, new URL("http://www.glbrc.org")));
         alRepo.save(new AppLink("WEI", "Wisconsin Energy Institute", "Green", false, new URL("https://energy.wisc.edu/")));
-        alRepo.save(new AppLink("Twitter", "Twitter", "Purple", false, new URL("https://twitter.com/")));
+        alRepo.save(new AppLink("Twitter", "Social Media", "Purple", false, new URL("https://twitter.com/")));
+        alRepo.save(new AppLink("Facebook", "Social Media", "CornflowerBlue", true, new URL("https://facebook.com/")));
+        alRepo.save(new AppLink("LinkedIn", "Social Media", "DeepSkyBlue", true, new URL("https://linkedin.com/")));
+        alRepo.save(new AppLink("Google Maps", "Navigation", "LimeGreen", true, new URL("https://google.com/maps")));
+        alRepo.save(new AppLink("Wolfram Alpha", "Calculator", "Orange", true, new URL("http://m.wolframalpha.com/")));
+        alRepo.save(new AppLink("Weather", "Weather", "SkyBlue", true, new URL("https://weather.com/")));
+        alRepo.save(new AppLink("SpotHero", "Parking", "RoyalBlue", true, new URL("https://spothero.com/")));
+        alRepo.save(new AppLink("Wikipedia", "Encyclopedia", "Grey", true, new URL("https://www.wikipedia.org/")));
+        alRepo.save(new AppLink("UW Credit Union", "Banking", "OrangeRed", true, new URL("https://www.uwcu.org/")));
+        alRepo.save(new AppLink("Yelp", "Consumer Reviews", "Salmon", true, new URL("https://www.yelp.com/")));
+        alRepo.save(new AppLink("Indeed", "Jobs", "Aquamarine", true, new URL("https://www.indeed.com/")));
     }
 
     /**
