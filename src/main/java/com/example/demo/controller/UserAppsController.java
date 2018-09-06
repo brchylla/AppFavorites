8964e5b0aecd113feb8363d2f7bfb4e76876b37f
dev-demos <ejbgames@gmail.com>
2018-09-05 23:28:52 -0500

    Drag and drop attempt

--- src/main/java/com/example/demo/controller/UserAppsController.java
+++ src/main/java/com/example/demo/controller/UserAppsController.java
@@ -1,78 +1,193 @@
 package com.example.demo.controller;
 
+
+
 import com.example.demo.data.applink.AppLink;
+
 import com.example.demo.data.userapps.UserApps;
+
 import com.example.demo.data.userapps.UserAppsService;
+
+
 
 import java.util.List;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+
+
 /**
+
  * Created by ben chylla on 9/2/2018
+
  */
+
 @RestController
+
 public class UserAppsController {
+
 	
+
 	@Autowired
+
 	UserAppsService uaService;
+
 	
+
 	@RequestMapping(value="/getUser", method=RequestMethod.GET)
+
 	public UserApps getUserApps(@RequestParam(value="login") String login) {
+
 		return uaService.getUser(login);
+
 	}
+
 	
-	/**
-	 * Return list of app links open for this user
-	 * @param login				The user login
-	 * @return					All app links open for this user
-	 */
-	@RequestMapping(value="/getOpenAppLinks", method=RequestMethod.GET)
-    public List<AppLink> getOpenAppLinks(@RequestParam(value="login") String login) {
-		return uaService.getOpenAppLinks(login);
-    }
 
 	/**
-	 * Return list of app links closed for this user
+
+	 * Return list of app links open for this user
+
 	 * @param login				The user login
-	 * @return					All app links closed for this user
+
+	 * @return					All app links open for this user
+
 	 */
-	@RequestMapping(value="/getClosedAppLinks", method=RequestMethod.GET)
-    public List<AppLink> getClosedAppLinks(@RequestParam(value="login") String login) {
-    	return uaService.getClosedAppLinks(login);
-    }
-	
-	/**
-	 * Remove app link from view for a specific user
-	 * @param appName			The name of the app link
-	 * @param login				The login of the user
-	 * @return					Updated list of app links to open in user view
-	 */
-	@RequestMapping(value="/removeAppLink", method=RequestMethod.GET)
-    public List<AppLink> removeAppLink(@RequestParam(value="appName") String appName, 
-    								   @RequestParam(value="login") String login) {
-		// update user table by removing app link name
-		uaService.removeAppLink(appName, login);
-		// return updated list of open app links
+
+	@RequestMapping(value="/getOpenAppLinks", method=RequestMethod.GET)
+
+    public List<AppLink> getOpenAppLinks(@RequestParam(value="login") String login) {
+
 		return uaService.getOpenAppLinks(login);
+
     }
+
+
+
+	/**
+
+	 * Return list of app links closed for this user
+
+	 * @param login				The user login
+
+	 * @return					All app links closed for this user
+
+	 */
+
+	@RequestMapping(value="/getClosedAppLinks", method=RequestMethod.GET)
+
+    public List<AppLink> getClosedAppLinks(@RequestParam(value="login") String login) {
+
+    	return uaService.getClosedAppLinks(login);
+
+    }
+
+	
+
+	/**
+
+	 * Remove app link from view for a specific user
+
+	 * @param appName			The name of the app link
+
+	 * @param login				The login of the user
+
+	 * @return					Updated list of app links to open in user view
+
+	 */
+
+	@RequestMapping(value="/removeAppLink", method=RequestMethod.GET)
+
+    public List<AppLink> removeAppLink(@RequestParam(value="appName") String appName, 
+
+    								   @RequestParam(value="login") String login) {
+
+		// update user table by removing app link name
+
+		uaService.removeAppLink(appName, login);
+
+		// return updated list of open app links
+
+		return uaService.getOpenAppLinks(login);
+
+    }
+
+
 
     /**
+
      * Add app link to view for a specific user
+
      * @param appName			The name of the app link
+
      * @param login				The login of the user
+
      * @return					Updated list of app links to open in user view
+
      */
+
 	@RequestMapping(value="/addAppLink", method=RequestMethod.GET)
+
     public List<AppLink> addAppLink(@RequestParam(value="appName") String appName, 
+
 									@RequestParam(value="login") String login) {
+
     	// update user table by adding app link name
+
     	uaService.addAppLink(appName, login);
+
     	// return updated list of open app links
+
     	return uaService.getOpenAppLinks(login);
+
     }
+
+	
+
+	/**
+
+	 * Reorder app links for a specific user
+
+	 * @param A JSON array of the names of the links
+
+	 * @return 
+
+	 */
+
+	@RequestMapping(value="/setUserAppsOrder", method=RequestMethod.POST)
+
+	public ResponseEntity<UserApps> setUserAppsOrder(@RequestBody UserApps userUpdate) {
+
+		// find user
+
+		UserApps user = uaService.getUser(userUpdate.getLogin());
+
+		user.setAppNames(userUpdate.getAppNames());
+
+		uaService.updateUser(user);
+
+		return new ResponseEntity<UserApps>(user, HttpStatus.OK);
+
+	}
+
+	
+
 }
--- src/main/java/com/example/demo/data/userapps/UserAppsService.java
+++ src/main/java/com/example/demo/data/userapps/UserAppsService.java
@@ -1,64 +1,131 @@
 package com.example.demo.data.userapps;
 
+
+
 import java.util.List;
+
+
 
 import com.example.demo.data.applink.AppLink;
 
+
+
 public interface UserAppsService {
+
 	UserAppsRepository getRepository();
+
 	
+
 	/**
+
 	 * Initialize all users (first time usage)
+
 	 */
+
 	void initialize();
+
 	
+
 	/**
+
 	 * Get user with corresponding login
+
 	 * @param login		The user login
+
 	 * @return			The user with the given login
+
 	 */
+
 	UserApps getUser(String login);
+
 	
+
 	/**
+
 	 * Get all users
+
 	 * @return	All users
+
 	 */
+
 	List<UserApps> getAllUsers();
+
 	
+
 	/**
+
 	 * Get all app links open to user
+
 	 * @param login		The login of the current user
+
 	 * @return			The app links open to the user
+
 	 */
+
 	List<AppLink> getOpenAppLinks(String login);
+
 	
+
 	/**
+
 	 * Get all app links NOT open to user
+
 	 * @param login		The login of the current user
+
 	 * @return			The app links NOT open to the user
+
 	 */
+
 	List<AppLink> getClosedAppLinks(String login);
+
 	
+
 	/**
+
 	 * Remove app link with given name from user table corresponding to login
+
 	 * @param appName	The name of the app to be removed
+
 	 * @param login		The login of the user whose table the app is to be removed from
+
 	 */
+
 	void removeAppLink(String appName, String login);
+
 	
+
 	/**
+
 	 * Add app link with given name to user table corresponding to login
+
 	 * @param appName	The name of the app to be added
+
 	 * @param login		The login of the user whose table the app is to be added to
+
 	 */
+
 	void addAppLink(String appName, String login);
+
 	
+
 	/**
+
 	 * Get app link with given name if it is open to user with given login
+
 	 * @param login		The login of the current user
+
 	 * @param name		The name of the app
+
 	 * @return			The app link with the given name (or null)
+
 	 */
+
 	AppLink getAppLinkByName(String login, String appName);
+
 	
+
+	void updateUser(UserApps user);
+
+	
+
 }
